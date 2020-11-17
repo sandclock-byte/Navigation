28e38c3cca239e15f7911cdcd9afc7e8b66ff541
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-16 20:35:59 -0600

    Initial commit

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../screens/Home';
+import Contact from '../screens/Contact';
+import About from '../screens/About';
+
+const Stack = createStackNavigator();
+
+export default function Navigation() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='home' component={Home} options={{title: 'Home'}}/>
+            <Stack.Screen name='about' component={About} options={{title: 'About'}}/>
+            <Stack.Screen name='contact' component={Contact} options={{title: 'Contact'}}/>
+        </Stack.Navigator>
+    )
+}
