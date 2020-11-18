7f360b259b47c36bace151631ffcfb10b0c0626c
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-18 10:16:39 -0600

    Se navega entre pantallas

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function About() {
+export default function About(props) {
+
+    const { navigation } = props;
+
     return (
         <View>
             <Text>Estamos en ABOUT</Text>
+
+            <Button title='Ir a Home' onPress={() => navigation.navigate('home')} />
+            <Button title='Ir a Contact' onPress={() => navigation.navigate('contact')} />
         </View>
     )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function Home() {
+export default function Home(props) {
+    const { navigation } = props;
     return (
         <View>
             <Text>Estamos en la HOME</Text>
+
+            <Button title='Ir a About' onPress={() => navigation.navigate('about')} />
+            <Button title='Ir a Contact' onPress={() => navigation.navigate('contact')} />
         </View>
     )
 }
